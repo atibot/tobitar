812e98270de26098deefba2aee9e9acee68dc214
Hideh Tobita <h-tobita@tkc.att.ne.jp>
2017-10-15 15:18:01 +0900

    temp test

--- tests/test_all.R
+++ tests/test_all.R
@@ -1,3 +1,3 @@
 library(testthat)
-source("~/MY Learning/Mastering Software Development in R/PackageDev/tobitar/R/fars_functions.R")
+source("R/fars_functions.R")
 expect_that(make_filename(2013), matches("accident_2013.csv.bz2"))
